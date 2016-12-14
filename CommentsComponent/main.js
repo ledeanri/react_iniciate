909438ddd82dff9ccd476491db50ce8d7cbc24a3
leandro <ledeanri@gmail.com>
2016-12-14 10:42:12 -0200

    react iniciate book

---
+++ CommentsComponent/main.js
@@ -0,0 +1,74 @@
+var data = [
+  {"author": "Ted1", "text": "First"},
+  {"author": "Daniel1", "text": "Hello World"}
+];
+
+var Panel = React.createClass({
+  render: function(){
+    return(
+      <div className="panel panel-default comment">
+      <div className="panel-heading">
+      {this.props.title}
+      </div>
+      <div className="panel-body">
+      {this.props.children}
+      </div>
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function(){
+    return(
+      <Panel title={this.props.author}>
+      {this.props.children}
+      </Panel>
+    );
+  }
+});
+
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+      {this.props.data.map(function(c){
+        return <Comment author={c.author}>
+        {c.text}
+        </Comment>;
+      })}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function(){
+    return(
+      <div className="CommentForm">
+      CommentForm
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function(){
+    return(
+      <div className="commentBox">
+      <Panel title="Comments">
+      <CommentList data={this.props.data} />
+      </Panel>
+      <Panel title="Add a Comment">
+      <CommentForm />
+      </Panel>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox data={data} />,
+  document.getElementById("content")
+);
